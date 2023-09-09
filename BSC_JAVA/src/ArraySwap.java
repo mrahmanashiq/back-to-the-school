798688b950ea81ff6d4a8069e43f3e7e0e5ce6af
mrahmanashiq <mizanur.r.ashiq@gmail.com>
2023-09-09 12:43:48 +0600

    maximum value code added

--- BSC_JAVA/src/ArraySwap.java
+++ BSC_JAVA/src/ArraySwap.java
@@ -8,7 +8,7 @@ public class ArraySwap {
         System.out.println(Arrays.toString(arr1));
     }
 
-    public static void swap(int[] arr, int index1, int index2) {
+     public static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
--- BSC_JAVA/src/MaxNumber.java
+++ BSC_JAVA/src/MaxNumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class MaxNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
